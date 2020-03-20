3792eb7dbab35bedd9ca88b7b242f43cfec48cb0
Shivam Patel <46139029+Shivam1615@users.noreply.github.com>
2020-03-19 21:29:48 -0400

    Add files via upload

---
+++ International.java
@@ -0,0 +1,97 @@
+/**
+ * 
+ * This class overrides toString, tuitionDue and includes testbed main class to
+ * test all of the methods of an international student.
+ * 
+ * @author Shivam Patel
+ * @author Kevin Shah
+ */
+public class International extends Student {
+    private boolean exchange;
+
+    /**
+     * This constructor initializes the first name, last name, number of credits,
+     * and exchange variable.
+     * 
+     * @param fname    is the first name.
+     * @param lname    is the last name.
+     * @param credit   is the number of credits the student is taking.
+     * @param exchange is the status of whether international student is an exchange
+     *                 student or not.
+     */
+    public International(String fname, String lname, int credit, boolean exchange) {
+        super(fname, lname, credit);
+        this.exchange = exchange;
+    }
+
+    /**
+     * This toString() method overrides the toString() method of the Student class
+     * to include tuition amount of an international student.
+     */
+    @Override
+    public String toString() {
+        return super.toString() + " tuition due: " + "$" + this.tuitionDue();
+    }
+
+    /**
+     * This tuitionDue() is overriding the abstract method of the Student class to
+     * compute how much tuition the international student has to pay.
+     */
+    @Override
+    public int tuitionDue() {
+        int tuition = 0;
+
+        if (this.credit < 12) {
+            if (this.exchange == true) {
+                tuition = Student.UNIVERSITYFEE_FULLTIME + Student.INTERNATIONAL_STUDENT_FEE;
+            } else if (this.exchange == false) {
+                tuition = (Student.INTERNATIONAL_PERCOST * this.credit) + Student.UNIVERSITYFEE_PARTTIME
+                        + Student.INTERNATIONAL_STUDENT_FEE;
+            }
+        } else if (this.credit >= 12) {
+            if (this.credit >= 15) {
+                if (this.exchange == true) {
+                    tuition = Student.UNIVERSITYFEE_FULLTIME + Student.INTERNATIONAL_STUDENT_FEE;
+                } else if (this.exchange == false) {
+                    tuition = (Student.INTERNATIONAL_PERCOST * 15) + Student.UNIVERSITYFEE_FULLTIME
+                            + Student.INTERNATIONAL_STUDENT_FEE;
+                }
+            } else {
+                if (this.exchange == true) {
+                    tuition = Student.UNIVERSITYFEE_FULLTIME + Student.INTERNATIONAL_STUDENT_FEE;
+                } else if (this.exchange == false) {
+                    tuition = (Student.INTERNATIONAL_PERCOST * this.credit) + Student.UNIVERSITYFEE_FULLTIME
+                            + Student.INTERNATIONAL_STUDENT_FEE;
+                }
+            }
+        }
+        return tuition;
+
+    }
+
+    /**
+     * This testbed main method tests all of the constructors and all of the methods
+     * of the class.
+     * 
+     * @param args is the main argument of the testbed method.
+     */
+    public static void main(String[] args) {
+
+        International student = new International("Mary", "Yang", 17, true);
+        International student2 = new International("Dhanush", "Gandham", 9, false);
+        International student3 = new International("Ken", "Liang", 12, false);
+        International student4 = new International("Ken", "Liang", 13, true);
+
+        System.out.println(student.toString());
+        System.out.println(student2.toString());
+        System.out.println(student3.toString());
+        System.out.println(student4.toString());
+
+        System.out.println(student.tuitionDue());
+        System.out.println(student2.tuitionDue());
+        System.out.println(student3.tuitionDue());
+        System.out.println(student4.tuitionDue());
+
+    }
+
+}
---
+++ Student.java
@@ -0,0 +1,86 @@
+/**
+ * This is an abstract class which includes an abstract method called tuition
+ * due that its subclasses inherit. This class also includes the compareto
+ * method in order to compare the first name and the last name of a student.
+ * 
+ * @author Shivam Patel
+ * @author Kevin Shah
+ */
+public abstract class Student implements Comparable<Object> {
+	private String fname;
+	private String lname;
+	protected int credit;
+
+	public static final int INSTATE_PERCOST = 433;
+	public static final int OUTSTATE_PERCOST = 756;
+	public static final int INTERNATIONAL_PERCOST = 945;
+	public static final int UNIVERSITYFEE_PARTTIME = 846;
+	public static final int UNIVERSITYFEE_FULLTIME = 1441;
+	public static final int INTERNATIONAL_STUDENT_FEE = 350;
+
+	/**
+	 * This constuctor initializes the first name, last name, and the number of
+	 * credits for all subclasses to inherit.
+	 * 
+	 * @param fname  is first name of the student.
+	 * @param lname  is last name of the student.
+	 * @param credit is number of credits student decides to take.
+	 */
+	public Student(String fname, String lname, int credit) { // constructor
+		this.fname = fname;
+		this.lname = lname;
+		this.credit = credit;
+	}
+
+	// must implement compareTo method because Student class implements the
+	// Comparable Interface
+	// return 0 if fname and lname of the two students are equal,
+	// return -1 if this fname and lname is < obj’s, return 1 if this fname and
+	// lname is > obj’s
+	// Hint: use the .equals and compareToIgnoreCase methods of the String class to
+	// compare fname
+	// and lname; names are not case-sensitive
+	/**
+	 * The compareTo method checks whether the first name and the last name of the
+	 * student match in the list and returns 0 if the names do match.
+	 */
+	public int compareTo(Object obj) {
+		Student student = (Student) obj;
+		if (this.fname.equalsIgnoreCase(student.fname) && this.lname.equalsIgnoreCase(student.lname)) {
+			return 0;
+		}
+
+		if (this.fname.compareToIgnoreCase(student.fname) < 0 && this.lname.compareToIgnoreCase(student.lname) < 0) {
+			return -1;
+		}
+
+		if (this.fname.compareToIgnoreCase(student.fname) > 0 && this.lname.compareToIgnoreCase(student.lname) > 0) {
+			return 1;
+		}
+
+		return -1;
+	}
+
+	// return a string with fname, lname and credit hours; subclasses will be using
+	// this method.
+	/**
+	 * This method returns the string of the first name, last name, and the number
+	 * of credits student is taking.
+	 */
+	public String toString() {
+		return this.fname + " " + this.lname + " " + this.credit;
+	}
+
+	// compute the tuition due; concrete implementation is required in the
+	// subclasses.
+	/**
+	 * This is the abstract method and computes the tuition amount by the subclasses
+	 * in order to compute the tuition amounts by the respective subclasses of
+	 * instate, outstate, and international.
+	 * 
+	 * @return returns an integer value after subclasses inherit this abstract
+	 *         method.
+	 */
+	public abstract int tuitionDue();
+
+}
